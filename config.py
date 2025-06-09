9faab21497083e2b56e128ef2fed9fff4e3e52f5
tyagishubham177 <tyagi.shubham177@gmail.com>
2025-06-09 19:33:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 # ——————————————————————————————————————————
 # Config from GitHub Secrets / .env
-URL       = ("https://shop.amul.com/en/product/"
-             "amul-adrak-chai-instant-tea-mix-14-g-or-pack-of-10-sachets")
+URL       = ("https://shop.amul.com/en/product/amul-high-protein-rose-lassi-200-ml-or-pack-of-30")
 PINCODE   = os.getenv("PINCODE",  "110001")
 # F2S_KEY   = os.getenv("F2S_API_KEY")             # Fast2SMS auth key
 # F2S_TO    = os.getenv("F2S_NUMBERS")             # Comma-separated numbers (e.g. 91xxxxxxxxxx)
