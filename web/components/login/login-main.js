916719f9f4047efcb0b0e4016883f937c7d048c6
tyagishubham177 <tyagi.shubham177@gmail.com>
2025-06-14 00:52:20 +0530

    Fix background not showing on login page

--- web/components/login/login-main.js
+++ web/components/login/login-main.js
@@ -1,9 +1,11 @@
 import { initLogin } from './login.js';
 import { initParticles } from '../particles-config/particles-config.js';
 import { initIcons } from '../icons/icons.js';
+import { initBackground } from '../ui/ui.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initLogin();
   initParticles();
+  initBackground();
   initIcons();
 });
