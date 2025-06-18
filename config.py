0839f98a68ed41ad1d9804d6a663566851873859
tyagishubham177 <tyagi.shubham177@gmail.com>
2025-06-18 11:50:54 +0530

    Fix spacing issues and remove redundant length check

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 # ——————————————————————————————————————————
 # Config from GitHub Secrets / .env
-URL       = ("https://shop.amul.com/en/product/amul-high-protein-rose-lassi-200-ml-or-pack-of-30")
-PINCODE   = os.getenv("PINCODE",  "110001")
+URL = "https://shop.amul.com/en/product/amul-high-protein-rose-lassi-200-ml-or-pack-of-30"
+PINCODE = os.getenv("PINCODE", "110001")
 # F2S_KEY   = os.getenv("F2S_API_KEY")             # Fast2SMS auth key
 # F2S_TO    = os.getenv("F2S_NUMBERS")             # Comma-separated numbers (e.g. 91xxxxxxxxxx)
 EMAIL_HOST = os.getenv("EMAIL_HOST")
