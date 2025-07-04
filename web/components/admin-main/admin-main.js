a217a730019bea20a1e99a94588f5a287069584b
tyagishubham177 <tyagi.shubham177@gmail.com>
2025-07-04 18:55:52 +0530

    Add default pincode option for email blast

--- web/components/admin-main/admin-main.js
+++ web/components/admin-main/admin-main.js
@@ -123,6 +123,8 @@ document.addEventListener('DOMContentLoaded', () => {
             baseRecipients = recips.filter(r => activeSubs.has(r.id)).map(r => r.email);
           } else if (type === 'paused-subscribers') {
             baseRecipients = recips.filter(r => pausedSubs.has(r.id)).map(r => r.email);
+          } else if (type === 'default-pincode') {
+            baseRecipients = recips.filter(r => r.pincode === '201305').map(r => r.email);
           } else {
             baseRecipients = [];
           }
